640f4a919527ae0c4de1d92aaf386bd2299fa69c
l-fujiwara <39792865+l-fujiwara@users.noreply.github.com>
2019-06-04 14:50:00 +0900

    Update main.py

--- camera/main.py
+++ camera/main.py
@@ -25,9 +25,9 @@ def gen(camera):
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 #@app.route('/video_feed')
-#def video_feed():
-#    return Response(gen(video_camera),
-#                    mimetype='multipart/x-mixed-replace; boundary=frame')
+def video_feed():
+    return Response(gen(video_camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', debug=False, threaded=True)
