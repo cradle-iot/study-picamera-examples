05da0cf7b1c07c206ba286043bfdb0c020459bcc
l-fujiwara <39792865+l-fujiwara@users.noreply.github.com>
2019-06-04 16:00:31 +0900

    Update main.py

--- camera/main.py
+++ camera/main.py
@@ -32,5 +32,5 @@ def video_feed():
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', debug=False, threaded=True)
-    print('running...')
-    video_feed()
+    while True:
+        video_camera
