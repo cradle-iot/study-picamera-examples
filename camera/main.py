bef237d3c80769d688c0c6fad2a942ea795d31a4
l-fujiwara <39792865+l-fujiwara@users.noreply.github.com>
2019-06-04 15:50:44 +0900

    Update main.py

--- camera/main.py
+++ camera/main.py
@@ -20,8 +20,8 @@ video_camera = VideoCamera(flip=False)
 
 def gen(camera):
     while True:
-        frame = camera.get_frame().process_image()
-        
+        frame = camera.get_frame()
+        camera.process_image(self, frame)
         #yield (b'--frame\r\n'
         #       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
