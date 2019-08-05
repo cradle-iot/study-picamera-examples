235db7cfc109a87e005dcb8bb539d8d5283e4a38
l-fujiwara <39792865+l-fujiwara@users.noreply.github.com>
2019-08-05 14:31:07 +0900

    import putDynamo やめて ここに書いた

--- camera/processor/person_detector.py
+++ camera/processor/person_detector.py
@@ -8,10 +8,10 @@ import numpy as np
 import cv2
 
 import os
-from putDynamo import insert
 import datetime
 import decimal
 import json
+import boto3
 
 print('starting... model reading...')
 net = cv2.dnn.readNetFromCaffe('/var/isaax/project/camera/processor/MobileNetSSD_deploy.prototxt',
@@ -90,4 +90,32 @@ class PersonDetector(object):
 obj = ["background", "aeroplane", "bicycle", "bird", "boat",
        "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
        "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-       "sofa", "train", "tvmonitor"]  
+       "sofa", "train", "tvmonitor"] 
+
+def insert(items):
+    # データベース接続の初期化
+    session = boto3.session.Session(
+                                    region_name = os.environ['REGION']
+                                    aws_access_key_id = os.environ['A_KEY']
+                                    aws_secret_access_key = os.environ['S_KEY']
+                                    )
+    dynamodb = session.resource('dynamodb')
+
+
+    # テーブルと接続
+    table_name = os.environ['TABLE_NAME']
+    table = dynamodb.Table(table_name)
+
+    for item in items:
+        # 追加する
+        response = table.put_item(
+            TableName=table_name,
+            Item=item
+        )
+        if response['ResponseMetadata']['HTTPStatusCode'] is not 200:
+            # 失敗処理
+            print(response)
+        else:
+            # 成功処理
+            print('Successed :', item['device'])
+    return
