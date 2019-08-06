12d0cd941701a6fb75dca25815d7ef0e7c12c524
lanvu fujiwara <fujiwara@d-cradle.or.jp>
2019-08-06 15:45:04 +0900

    Dynamo

--- camera/simple_detector.py
+++ camera/simple_detector.py
@@ -13,6 +13,34 @@ import decimal
 import json
 import boto3
 
+def insert(items):
+    # データベース接続の初期化
+    session = boto3.session.Session(
+                                    region_name = os.environ['REGION'],
+                                    aws_access_key_id = os.environ['A_KEY'],
+                                    aws_secret_access_key = os.environ['S_KEY'],
+                                    )
+    dynamodb = session.resource('dynamodb')
+
+
+    # テーブルと接続
+    table_name = os.environ['TABLE_NAME']
+    table = dynamodb.Table(table_name)
+
+    for item in items:
+        # 追加する
+        response = table.put_item(
+            TableName=table_name,
+            Item=item
+        )
+        if response['ResponseMetadata']['HTTPStatusCode'] is not 200:
+            # 失敗処理
+            print(response)
+        else:
+            # 成功処理
+            print('Successed :', item['device'])
+    return
+
 obj = ["background", "aeroplane", "bicycle", "bird", "boat",
        "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
        "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
@@ -71,4 +99,6 @@ while True:
         
     if len(data_list) > 10:
         print(data_list)
+        insert(data_list)
         data_list = []
+        
