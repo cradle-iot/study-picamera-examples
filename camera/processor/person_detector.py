6a144466dd322a30b66f277060f5288a8a45ac33
l-fujiwara <39792865+l-fujiwara@users.noreply.github.com>
2019-09-13 14:10:27 +0900

    Update person_detector.py

--- camera/processor/person_detector.py
+++ camera/processor/person_detector.py
@@ -9,9 +9,7 @@ import cv2
 
 import os
 import datetime
-import decimal
 import json
-import boto3
 
 print('starting... model reading...')
 net = cv2.dnn.readNetFromCaffe('/var/isaax/project/camera/processor/MobileNetSSD_deploy.prototxt',
@@ -66,59 +64,15 @@ class PersonDetector(object):
             y = startY - 15 if startY - 15 > 15 else startY + 15
             cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
             count_list[idx] += 1
-        
-            data = {}
-            data['data'] = {'x': (endX-startX)/2, 'y':(endY-startY)/2}
-            data['timestamp'] = datetime.datetime.now().timestamp()
-            data['device'] = os.environ['DEVICE']
-            data['person_id'] = person_id
             person_id += 1
 
             for i in range(21):
                 if count_list[i] > 0 and i == 15:
                     print('Count_{}: {}'.format(obj[i], count_list[i]),datetime.datetime.now())
-#                    data['data'][obj[i]] = count_list[i]
-            
-            
-                
-#            data = json.dumps(data)
-#            data = json.loads(data, parse_float=decimal.Decimal)
-#            
-#            items = [data]
-#            if sum(count_list) > 0:
-#                insert(items)
-                    
+
         return frame
     
 obj = ["background", "aeroplane", "bicycle", "bird", "boat",
        "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
        "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
        "sofa", "train", "tvmonitor"] 
-
-def insert(items):
-    # データベース接続の初期化
-    session = boto3.session.Session(
-                                    region_name = os.environ['REGION'],
-                                    aws_access_key_id = os.environ['A_KEY'],
-                                    aws_secret_access_key = os.environ['S_KEY'],
-                                    )
-    dynamodb = session.resource('dynamodb')
-
-
-    # テーブルと接続
-    table_name = os.environ['TABLE_NAME']
-    table = dynamodb.Table(table_name)
-
-    for item in items:
-        # 追加する
-        response = table.put_item(
-            TableName=table_name,
-            Item=item
-        )
-        if response['ResponseMetadata']['HTTPStatusCode'] is not 200:
-            # 失敗処理
-            print(response)
-        else:
-            # 成功処理
-            print('Successed :', item['device'])
-    return
